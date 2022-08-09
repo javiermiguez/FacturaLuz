71cae3babca219523ff332207404ac6be49c95c8
Javier Míguez Lalín <javiermiguezlalin@gmail.com>
2022-08-09 22:16:54 +0200

    Metín o código todo na MainPage. Descarga os arquivos de consumos e prezos. Falta filtrar o dos prezos e xa poderei calcular a factura.

--- FacturaLuz/ViewModels/MainViewModel.cs
+++ FacturaLuz/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -16,84 +17,145 @@ public class MainViewModel : ObservableRecipient
     {
     }
 
-    public static async Task<string> ObterConsumosAsync()
-    {
-        string response;
+    //public class Consumo
+    //{
+    //    public DateTime DataHora
+    //    {
+    //        get; set;
+    //    }
+    //    public float WatiosHora
+    //    {
+    //        get; set;
+    //    }
+    //}
 
-        var url = (ApplicationData.Current.LocalSettings.Values["urlMedidor"] as string);
+    //public static async Task ObterConsumosAsync(DateTimeOffset dataInicio, DateTimeOffset dataFin, Progress<double> progress, string response)
+    //{
+    //    dataInicio = new DateTimeOffset(dataInicio.Year, dataInicio.Month, dataInicio.Day, 0, 0, 0, TimeSpan.Zero);
+    //    dataFin = new DateTimeOffset(dataFin.Year, dataFin.Month, dataFin.Day, 23, 59, 59, TimeSpan.Zero);
 
-        if (url == null)
-        {
-            response = "\nA URL do medidor non está gardada na configuración.";
-        }
-        else
-        {
-            if (!url.EndsWith("/"))
-                url += "/";
+    //    var filePath = ApplicationData.Current.LocalFolder.Path + "\\em_data.csv";
+    //    var response = "\n- Comprobando se existe o arquivo de consumos: " + filePath;
+    //    bool necesarioDescargarArquivo;
 
-            url += "emeter/1/em_data.csv";
+    //    if (File.Exists(filePath))
+    //    {
+    //        response += "\n- Arquivo atopado. Comprobando se contén as datas requiridas...";
 
-            var progress = new Progress<double>();
-            progress.ProgressChanged += (sender, value) => System.Console.Write("\r%{0:N0}", value);
+    //        var consumos = FiltrarConsumos(dataInicio, dataFin, filePath);
 
-            var cancellationToken = new CancellationTokenSource();
+    //        var existeDataInicio = consumos.Any(c => c.DataHora.Date == dataInicio.Date && c.DataHora.Hour == 0);
+    //        var existeDataFin = consumos.Any(c => c.DataHora.Date == dataFin.Date && c.DataHora.Hour == 23);
 
-            try
-            {
-                await DownloadFileAsync(url, progress, cancellationToken.Token, "em_data.csv");
-                response = "\nDescargado o arquivo de consumos.";
-            }
-            catch (HttpRequestException e)
-            {
-                response = "\nProduciuse un erro: " + e.Message;
-            }
-        }
+    //        if (existeDataInicio && existeDataFin)
+    //        {
+    //            response += "\n- Atopados " + consumos.Count().ToString() + " rexistros entre as datas indicadas.";
+    //            necesarioDescargarArquivo = false;
+    //        }
+    //        else
+    //        {
+    //            response += "\n- Datas non atopadas. Procedendo coa descarga do arquivo...";
+    //            necesarioDescargarArquivo= true;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        response += "\n- Arquivo non atopado. Procedendo coa descarga...";
+    //        necesarioDescargarArquivo = true;
+    //    }
 
-        return response;
-    }
+    //    if (necesarioDescargarArquivo)
+    //    {
+    //        var url = (ApplicationData.Current.LocalSettings.Values["urlMedidor"] as string);
 
-    public static async Task DownloadFileAsync(string url, IProgress<double> progress, CancellationToken token, string filename)
-    {
-        var response = await App.httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+    //        if (url == null)
+    //        {
+    //            response += "\n- Atopouse un problema: A URL do medidor non está gardada na configuración.";
+    //        }
+    //        else
+    //        {
+    //            if (!url.EndsWith("/"))
+    //                url += "/";
 
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new Exception(string.Format("The request returned with HTTP status code {0}", response.StatusCode));
-        }
+    //            url += "emeter/1/em_data.csv";
 
-        var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
-        var canReportProgress = total != -1 && progress != null;
+    //            var cancellationToken = new CancellationTokenSource();
 
-        using var stream = await response.Content.ReadAsStreamAsync();
-        var totalRead = 0L;
-        var buffer = new byte[4096];
-        var isMoreToRead = true;
+    //            try
+    //            {
+    //                await DescargarArquivoAsync(url, filePath, progress, cancellationToken.Token);
+    //                response += "\n- Descarga completada.";
+    //            }
+    //            catch (HttpRequestException e)
+    //            {
+    //                response = "\n- Produciuse un erro durante a descarga: " + e.Message;
+    //            }
+    //        }
+    //    }
 
-        do
-        {
-            token.ThrowIfCancellationRequested();
+    //    return response;
+    //}
 
-            var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+    //private static List<Consumo> FiltrarConsumos(DateTimeOffset dataInicio, DateTimeOffset dataFin, string filePath)
+    //{
+    //    var csvLines = File.ReadLines(filePath)
+    //            .Skip(1)
+    //            .Where(s => s != "" && !s.StartsWith("D"))
+    //            .Select(s => s.Split(new[] { ',' }))
+    //            .Select(l => new Consumo
+    //            {
+    //                DataHora = DateTime.Parse(l[0]),
+    //                WatiosHora = float.Parse(l[1])
+    //            })
+    //            .Where(l => DateTimeOffset.Compare(l.DataHora, dataInicio) >= 0 && DateTimeOffset.Compare(l.DataHora, dataFin) <= 0).ToList();
 
-            if (read == 0)
-            {
-                isMoreToRead = false;
-            }
-            else
-            {
-                var data = new byte[read];
-                buffer.ToList().CopyTo(0, data, 0, read);
+    //    return csvLines;
+    //}
 
-                // TODO: put here the code to write the file to disk
-                File.WriteAllBytes(ApplicationData.Current.LocalFolder.Path + "\\" + filename, buffer);
-                
-                totalRead += read;
+    //public static async Task DescargarArquivoAsync(string url, string filePath, IProgress<double> progress, CancellationToken token)
+    //{
+    //    var response = await App.httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
 
-                if (canReportProgress)
-                {
-                    progress.Report((totalRead * 1d) / (total * 1d) * 100);
-                }
-            }
-        } while (isMoreToRead);
-    }
+    //    if (!response.IsSuccessStatusCode)
+    //    {
+    //        throw new Exception(string.Format("The request returned with HTTP status code {0}", response.StatusCode));
+    //    }
+
+    //    var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+    //    var canReportProgress = total != -1 && progress != null;
+        
+    //    using var stream = await response.Content.ReadAsStreamAsync();
+    //    var totalRead = 0L;
+    //    var buffer = new byte[4096];
+    //    var isMoreToRead = true;
+
+    //    do
+    //    {
+    //        token.ThrowIfCancellationRequested();
+
+    //        var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+
+    //        if (read == 0)
+    //        {
+    //            isMoreToRead = false;
+    //        }
+    //        else
+    //        {
+    //            var data = new byte[read];
+    //            buffer.ToList().CopyTo(0, data, 0, read);
+
+    //            var fileStream = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+    //            fileStream.Write(data);
+    //            fileStream.Close();
+    //            fileStream.Dispose();
+
+    //            totalRead += read;
+
+    //            if (canReportProgress)
+    //            {
+    //                progress.Report((totalRead * 1d) / (total * 1d) * 100);
+    //            }
+    //        }
+    //    } while (isMoreToRead);
+    //}
 }
--- FacturaLuz/Views/MainPage.xaml.cs
+++ FacturaLuz/Views/MainPage.xaml.cs
@@ -1,18 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using FacturaLuz.ViewModels;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using System.Text.Json;
 using Windows.Storage;
+using System.Globalization;
 
 namespace FacturaLuz.Views;
 
 public sealed partial class MainPage : Page
 {
+    private class Consumo
+    {
+        public DateTime DataHora
+        {
+            get; set;
+        }
+        public decimal WatiosHora
+        {
+            get; set;
+        }
+    }
+
+    private class Prezo
+    {
+        public DateTime DataHora
+        {
+            get; set;
+        }
+        public float PrezoWatioHora
+        {
+            get; set;
+        }
+    }
+
+    private List<Consumo> Consumos;
+
+    private List<Prezo> Prezos;
+
     public MainViewModel ViewModel
     {
         get;
@@ -22,6 +58,11 @@ public sealed partial class MainPage : Page
     {
         ViewModel = App.GetService<MainViewModel>();
         InitializeComponent();
+    }
+
+    private void abrirCartafolLocalButton_Click(object sender, RoutedEventArgs e)
+    {
+        Process.Start("explorer.exe", ApplicationData.Current.LocalFolder.Path);
     }
 
     private void finDatePicker_Opened(object sender, object e)
@@ -42,6 +83,275 @@ public sealed partial class MainPage : Page
 
     private async void calcularButton_Click(object sender, RoutedEventArgs e)
     {
-        infoRun.Text = await MainViewModel.ObterConsumosAsync();
+        infoRun.Text = "";
+
+        if (Validar())
+        {
+            progresoProgressRing.Visibility = Visibility.Visible;
+
+            infoRun.Text = "\n-------------------- INICIANDO PROCESO --------------------";
+
+            var dataInicio = new DateTime(inicioDatePicker.Date.Value.Year, inicioDatePicker.Date.Value.Month, inicioDatePicker.Date.Value.Day, 0, 0, 0);
+            var dataFin = new DateTime(inicioDatePicker.Date.Value.Year, inicioDatePicker.Date.Value.Month, inicioDatePicker.Date.Value.Day, 23, 59, 59);
+
+            await ObterConsumosAsync(dataInicio, dataFin);
+            await ObterPrezosAsync(dataInicio, dataFin);
+
+            CalcularFactura();
+        }
+    }
+
+    private bool Validar()
+    {
+        if (!inicioDatePicker.Date.HasValue || !finDatePicker.Date.HasValue)
+        {
+            validacionRun.Text = "Selecciona as datas de inicio e fin.";
+            return false;
+        }
+        else if (inicioDatePicker.Date > finDatePicker.Date)
+        {
+            validacionRun.Text = "A data de inicio ten que ser anterior ou igual á de fin.";
+            return false;
+        }
+        else if (finDatePicker.Date > DateTime.Now)
+        {
+            validacionRun.Text = "A data de fin ten que ser menor ou igual á de hoxe.";
+            return false;
+        }
+
+        validacionRun.Text = "";
+        return true;
+    }
+
+    private async Task ObterConsumosAsync(DateTime dataInicio, DateTime dataFin)
+    {
+        var rutaLocal = ApplicationData.Current.LocalFolder.Path + "\\em_data.csv";
+
+        infoRun.Text += "\n- Comprobando se existe o arquivo de consumos: " + rutaLocal;
+
+        bool necesarioDescargarArquivo;
+
+        if (File.Exists(rutaLocal))
+        {
+            infoRun.Text += "\n- Arquivo atopado. Comprobando se contén as datas requiridas...";
+
+            FiltrarConsumos(dataInicio, dataFin, rutaLocal);
+
+            var existeDataInicio = Consumos.Any(c => c.DataHora.Date == dataInicio.Date && c.DataHora.Hour == 0);
+            var existeDataFin = Consumos.Any(c => c.DataHora.Date == dataFin.Date && c.DataHora.Hour == 23);
+
+            if (existeDataInicio && existeDataFin)
+            {
+                infoRun.Text += "\n- Atopados " + Consumos.Count.ToString() + " rexistros entre as datas indicadas.";
+                necesarioDescargarArquivo = false;
+            }
+            else
+            {
+                infoRun.Text += "\n- Datas non atopadas.";
+                File.Delete(rutaLocal);
+                infoRun.Text += "\n- Arquivo eliminado. Procedendo coa descarga do arquivo...";
+                necesarioDescargarArquivo = true;
+            }
+        }
+        else
+        {
+            infoRun.Text += "\n- Arquivo non atopado. Procedendo coa descarga...";
+            necesarioDescargarArquivo = true;
+        }
+
+        if (necesarioDescargarArquivo)
+        {
+            var url = (ApplicationData.Current.LocalSettings.Values["urlMedidor"] as string);
+
+            if (url == null)
+            {
+                infoRun.Text += "\n- Atopouse un problema: A URL do medidor non está gardada na configuración.";
+            }
+            else
+            {
+                if (!url.EndsWith("/"))
+                {
+                    url += "/";
+                }
+
+                url += "emeter/1/em_data.csv";
+
+                var cancellationToken = new CancellationTokenSource();
+
+                try
+                {
+                    App.httpClient.DefaultRequestHeaders.Clear();
+                    await DescargarArquivoAsync(url, rutaLocal, cancellationToken.Token);
+                    infoRun.Text += "\n- Descarga completada.";
+                }
+                catch (HttpRequestException e)
+                {
+                    infoRun.Text = "\n- Produciuse un erro durante a descarga: " + e.Message;
+                }
+            }
+
+            FiltrarConsumos(dataInicio, dataFin, rutaLocal);
+        }
+    }
+
+    private async Task ObterPrezosAsync(DateTime dataInicio, DateTime dataFin)
+    {
+        var rutaLocal = ApplicationData.Current.LocalFolder.Path + "\\1001.json";
+
+        infoRun.Text += "\n\n- Comprobando se existe o arquivo de prezos: " + rutaLocal;
+
+        bool necesarioDescargarArquivo;
+
+        if (File.Exists(rutaLocal))
+        {
+            infoRun.Text += "\n- Arquivo atopado. Comprobando se contén as datas requiridas...";
+
+            FiltrarPrezos(dataInicio, dataFin, rutaLocal);
+
+            var existeDataInicio = Prezos.Any(p => p.DataHora.Date == dataInicio.Date && p.DataHora.Hour == 0);
+            var existeDataFin = Prezos.Any(p => p.DataHora.Date == dataFin.Date && p.DataHora.Hour == 23);
+
+            if (existeDataInicio && existeDataFin)
+            {
+                infoRun.Text += "\n- Atopados " + Prezos.Count.ToString() + " rexistros entre as datas indicadas.";
+                necesarioDescargarArquivo = false;
+            }
+            else
+            {
+                infoRun.Text += "\n- Datas non atopadas.";
+                File.Delete(rutaLocal);
+                infoRun.Text += "\n- Arquivo eliminado. Procedendo coa descarga do arquivo...";
+                necesarioDescargarArquivo = true;
+            }
+        }
+        else
+        {
+            infoRun.Text += "\n- Arquivo non atopado. Procedendo coa descarga...";
+            necesarioDescargarArquivo = true;
+        }
+
+        if (necesarioDescargarArquivo)
+        {
+            var url = "https://api.esios.ree.es/indicators/1001?"
+                + "start_date=" + dataInicio.Date.Year + "-" + dataInicio.Date.Month + "-" + dataInicio.Date.Day + "T00:00:00&"
+                + "end_date=" + dataFin.Date.Year + "-" + dataFin.Date.Month + "-" + dataFin.Date.Day + "T23:59:59&geo_ids[]=8741";
+
+            var cancellationToken = new CancellationTokenSource();
+
+            try
+            {
+                App.httpClient.DefaultRequestHeaders.Clear();
+                App.httpClient.DefaultRequestHeaders.Add("Host", "api.esios.ree.es");
+                App.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                App.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.esios-api-v2+json"));
+                App.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", @"token=""***REMOVED***""");
+                App.httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+
+                await DescargarArquivoAsync(url, rutaLocal, cancellationToken.Token);
+                infoRun.Text += "\n- Descarga completada.";
+            }
+            catch (HttpRequestException e)
+            {
+                infoRun.Text = "\n- Produciuse un erro durante a descarga: " + e.Message;
+            }
+
+            FiltrarPrezos(dataInicio, dataFin, rutaLocal);
+        }
+    }
+
+    private void FiltrarConsumos(DateTime dataInicio, DateTime dataFin, string rutaLocal)
+    {
+        Consumos = File.ReadLines(rutaLocal)
+                .Skip(1)
+                .Select(s => s.Split(new[] { ',' }))
+                .Select(c => new Consumo
+                {
+                    DataHora = TimeZoneInfo.ConvertTimeFromUtc(DateTime.Parse(c[0]), TimeZoneInfo.FindSystemTimeZoneById("Romance Standard Time")),
+                    WatiosHora = decimal.Parse(c[1], System.Globalization.NumberStyles.AllowDecimalPoint, new CultureInfo("en-US"))
+                })
+                .Where(c => c.DataHora >= dataInicio && c.DataHora <= dataFin)
+                .OrderBy(c => c.DataHora)
+                .ToList();
+    }
+
+    private void FiltrarPrezos(DateTimeOffset dataInicio, DateTimeOffset dataFin, string rutaLocal)
+    {
+        Prezos = new List<Prezo>();
+
+        using StreamReader streamReader = new StreamReader(rutaLocal);
+
+        var json = streamReader.ReadToEnd();
+        //Prezos = JsonSerializer.Deserialize<List<Prezo>>(json);
+        Console.WriteLine("Ola");
+    }
+
+    private void CalcularFactura()
+    {
+        //Product product = await response.Content.ReadAsAsync > Product > ();
+        //Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+    }
+
+    private async Task DescargarArquivoAsync(string url, string rutaLocal, CancellationToken token)
+    {
+        var response = await App.httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(string.Format("The request returned with HTTP status code {0}", response.StatusCode));
+        }
+
+        var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+        var canReportPercentProgress = total != -1;
+
+        using var stream = await response.Content.ReadAsStreamAsync();
+        var totalRead = 0L;
+        var buffer = new byte[4096];
+        var isMoreToRead = true;
+
+        if (!canReportPercentProgress)
+        {
+            progresoProgressRing.IsIndeterminate = true;
+            progresoProgressRing.IsActive = true;
+        }
+
+        do
+        {
+            token.ThrowIfCancellationRequested();
+
+            var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+
+            if (read == 0)
+            {
+                isMoreToRead = false;
+            }
+            else
+            {
+                var data = new byte[read];
+                buffer.ToList().CopyTo(0, data, 0, read);
+
+                var fileStream = new FileStream(rutaLocal, FileMode.Append, FileAccess.Write);
+                fileStream.Write(data);
+                fileStream.Close();
+                fileStream.Dispose();
+
+                totalRead += read;
+
+                if (canReportPercentProgress)
+                {
+                    progresoProgressRing.Value = (totalRead * 1d) / (total * 1d) * 100;
+                }
+                else
+                {
+                    descargaRun.Text = "\n" + totalRead.ToString() + " bytes descargados";
+                }
+            }
+        } while (isMoreToRead);
+
+        if (!canReportPercentProgress)
+        {
+            infoRun.Text += descargaRun.Text;
+            descargaRun.Text = "";
+            progresoProgressRing.IsActive = false;
+        }
     }
 }
